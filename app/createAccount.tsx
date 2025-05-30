9ea75f2a07f54af4f7aeaac5c64a16458db74cb9
Trey <treypresty@gmail.com>
2025-05-30 19:32:14 -0400

    routing fix error message addition

--- app/createAccount.tsx
+++ app/createAccount.tsx
@@ -48,6 +48,23 @@ const index = () => {
       return;
     }
 
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters long.");
+      return;
+    }
+
+      //Check if email exists
+      const emailQuery = query(
+        collection(db, 'users'),
+        where('email', '==', email)
+      );
+      const emailSnapshot = await getDocs(emailQuery);
+
+      if (!emailSnapshot.empty) {
+        alert("That email is already in use. Please choose another.");
+        return;
+      }
+
     //Account Creation
       const user = await createUserWithEmailAndPassword(auth, email, password); // take email and password and push to firebase
       const userCredential = user.user
@@ -70,7 +87,7 @@ const index = () => {
         });
       }
 
-      router.replace("/(tabs)/home")
+      router.replace("/selectMode")
 
     } catch (error: any) {
       console.log(error);
