881320ccb111d1874efbae9348229c6441fb3aa7
Trey <treypresty@gmail.com>
2025-05-31 18:56:46 -0400

    added group mode tab with basic method for group creation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -87,6 +87,27 @@ export default function TabLayout() {
         }}
       />
             <Tabs.Screen
+        name="groupMode"
+        options={{ 
+          title: 'Group Mode',
+          tabBarIcon: ({ color }) => <TabBarIcon name="sign-out" color={color} />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={25}
+                    color={Colors[colorScheme ?? 'light'].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+            <Tabs.Screen
         name="profile"
         options={{
           title: 'profile',
